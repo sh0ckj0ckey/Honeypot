891e4e0bd2ac22bb46c2b73f9358086edd51cdf1
姚一鸣 <yaoyiming123@live.com>
2023-12-24 18:44:23 +0800

    m

--- Honeypot.Core/SettingsService.cs
+++ Honeypot.Core/SettingsService.cs
@@ -13,11 +13,8 @@ namespace Honeypot.Core
         private const string SETTING_NAME_APPEARANCEINDEX = "AppearanceIndex";
         private const string SETTING_NAME_BACKDROPINDEX = "BackdropIndex";
         private const string SETTING_NAME_ENABLELOCK = "EnableLock";
+        private const string SETTING_NAME_ORDERMODE = "PasswordsOrderMode";
 
-        private const string SETTING_NAME_ENABLEGLOSSARY = "EnableGlossary";
-        private const string SETTING_NAME_AUTOCLEARLASTINPUT = "AutoClearLastInput";
-        private const string SETTING_NAME_CLOSEBUTTONMODE = "CloseButtonMode";
-        private const string SETTING_NAME_SEARCHBOXSTYLE = "SearchBoxStyle";
 
         private ApplicationDataContainer _localSettings = ApplicationData.Current.LocalSettings;
 
@@ -143,157 +140,43 @@ namespace Honeypot.Core
             }
         }
 
-        // 是否启用生词本
-        private bool? _enableGlossary = null;
-        public bool EnableGlossary
+        // 密码列表的排序方式 0-首字母 1-时间
+        private int _passwordsOrderMode = -1;
+        public int PasswordsOrderMode
         {
             get
             {
                 try
                 {
-                    if (_enableGlossary is null)
+                    if (_passwordsOrderMode < 0)
                     {
-                        if (_localSettings.Values[SETTING_NAME_ENABLEGLOSSARY] == null)
+                        if (_localSettings.Values[SETTING_NAME_ORDERMODE] == null)
                         {
-                            _enableGlossary = true;
+                            _passwordsOrderMode = 0;
                         }
-                        else if (_localSettings.Values[SETTING_NAME_ENABLEGLOSSARY]?.ToString() == "False")
+                        else if (_localSettings.Values[SETTING_NAME_ORDERMODE]?.ToString() == "0")
                         {
-                            _enableGlossary = false;
+                            _passwordsOrderMode = 0;
+                        }
+                        else if (_localSettings.Values[SETTING_NAME_ORDERMODE]?.ToString() == "1")
+                        {
+                            _passwordsOrderMode = 1;
                         }
                         else
                         {
-                            _enableGlossary = true;
+                            _passwordsOrderMode = 0;
                         }
                     }
                 }
                 catch { }
-                if (_enableGlossary is null) _enableGlossary = true;
-                return _enableGlossary != false;
+                if (_passwordsOrderMode < 0) _passwordsOrderMode = 0;
+                return _passwordsOrderMode < 0 ? 0 : _passwordsOrderMode;
             }
             set
             {
-                SetProperty(ref _enableGlossary, value);
-                ApplicationData.Current.LocalSettings.Values[SETTING_NAME_ENABLEGLOSSARY] = _enableGlossary;
+                SetProperty(ref _passwordsOrderMode, value);
+                ApplicationData.Current.LocalSettings.Values[SETTING_NAME_ORDERMODE] = _passwordsOrderMode;
             }
         }
-
-        // 是否自动清除上次输入
-        private bool? _autoClearLastInput = null;
-        public bool AutoClearLastInput
-        {
-            get
-            {
-                try
-                {
-                    if (_autoClearLastInput is null)
-                    {
-                        if (_localSettings.Values[SETTING_NAME_AUTOCLEARLASTINPUT] == null)
-                        {
-                            _autoClearLastInput = false;
-                        }
-                        else if (_localSettings.Values[SETTING_NAME_AUTOCLEARLASTINPUT]?.ToString() == "True")
-                        {
-                            _autoClearLastInput = true;
-                        }
-                        else
-                        {
-                            _autoClearLastInput = false;
-                        }
-                    }
-                }
-                catch { }
-                if (_autoClearLastInput is null) _autoClearLastInput = false;
-                return _autoClearLastInput == true;
-            }
-            set
-            {
-                SetProperty(ref _autoClearLastInput, value);
-                ApplicationData.Current.LocalSettings.Values[SETTING_NAME_AUTOCLEARLASTINPUT] = _autoClearLastInput;
-            }
-        }
-
-        // 设置关闭按钮的作用 0-隐藏 1-退出
-        private int _closeButtonMode = -1;
-        public int CloseButtonMode
-        {
-            get
-            {
-                try
-                {
-                    if (_closeButtonMode < 0)
-                    {
-                        if (_localSettings.Values[SETTING_NAME_CLOSEBUTTONMODE] == null)
-                        {
-                            _closeButtonMode = 0;
-                        }
-                        else if (_localSettings.Values[SETTING_NAME_CLOSEBUTTONMODE]?.ToString() == "0")
-                        {
-                            _closeButtonMode = 0;
-                        }
-                        else if (_localSettings.Values[SETTING_NAME_CLOSEBUTTONMODE]?.ToString() == "1")
-                        {
-                            _closeButtonMode = 1;
-                        }
-                        else
-                        {
-                            _closeButtonMode = 0;
-                        }
-                    }
-                }
-                catch { }
-                if (_closeButtonMode < 0) _closeButtonMode = 0;
-                return _closeButtonMode < 0 ? 0 : _closeButtonMode;
-            }
-            set
-            {
-                SetProperty(ref _closeButtonMode, value);
-                ApplicationData.Current.LocalSettings.Values[SETTING_NAME_CLOSEBUTTONMODE] = _closeButtonMode;
-            }
-        }
-
-        // 搜索框样式
-        private int _searchBoxStyle = -1;
-        public int SearchBoxStyle
-        {
-            get
-            {
-                try
-                {
-                    if (_searchBoxStyle < 0)
-                    {
-                        if (_localSettings.Values[SETTING_NAME_SEARCHBOXSTYLE] == null)
-                        {
-                            _searchBoxStyle = 2;
-                        }
-                        else if (_localSettings.Values[SETTING_NAME_SEARCHBOXSTYLE]?.ToString() == "0")
-                        {
-                            _searchBoxStyle = 0;
-                        }
-                        else if (_localSettings.Values[SETTING_NAME_SEARCHBOXSTYLE]?.ToString() == "1")
-                        {
-                            _searchBoxStyle = 1;
-                        }
-                        else if (_localSettings.Values[SETTING_NAME_SEARCHBOXSTYLE]?.ToString() == "2")
-                        {
-                            _searchBoxStyle = 2;
-                        }
-                        else
-                        {
-                            _searchBoxStyle = 2;
-                        }
-                    }
-                }
-                catch { }
-                if (_searchBoxStyle < 0) _searchBoxStyle = 2;
-                return _searchBoxStyle < 0 ? 2 : _searchBoxStyle;
-            }
-            set
-            {
-                SetProperty(ref _searchBoxStyle, value);
-                ApplicationData.Current.LocalSettings.Values[SETTING_NAME_SEARCHBOXSTYLE] = _searchBoxStyle;
-            }
-        }
-
     }
 }
