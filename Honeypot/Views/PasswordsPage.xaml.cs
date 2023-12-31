225870b532dbc90886c16e0ed8b96d1df26ac476
姚一鸣 <yaoyiming123@live.com>
2023-12-31 12:24:04 +0800

    m

--- Honeypot/Views/PasswordsPage.xaml.cs
+++ Honeypot/Views/PasswordsPage.xaml.cs
@@ -13,6 +13,11 @@ using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using Honeypot.ViewModels;
+using static System.Net.Mime.MediaTypeNames;
+using Windows.ApplicationModel.DataTransfer;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -33,9 +38,92 @@ namespace Honeypot.Views
             MainViewModel = MainViewModel.Instance;
         }
 
+        /// <summary>
+        /// 点击关闭页面
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void OnClickClose(object sender, RoutedEventArgs e)
         {
             MainViewModel.SelectedPassword = null;
         }
+
+        /// <summary>
+        /// 点击复制账号
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void OnClickCopyAccount(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(MainViewModel.Instance.SelectedPassword.Account))
+            {
+                DataPackage dataPackage = new();
+                dataPackage.SetText(MainViewModel.Instance.SelectedPassword.Account);
+                Clipboard.SetContent(dataPackage);
+
+                AccountButtonTextBlock.Visibility = Visibility.Collapsed;
+                AccountCopiedFontIcon.Visibility = Visibility.Visible;
+                await Task.Delay(1000);
+                AccountButtonTextBlock.Visibility = Visibility.Visible;
+                AccountCopiedFontIcon.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        /// <summary>
+        /// 点击复制密码
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void OnClickCopyPassword(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(MainViewModel.Instance.SelectedPassword.Password))
+            {
+                DataPackage dataPackage = new();
+                dataPackage.SetText(MainViewModel.Instance.SelectedPassword.Password);
+                Clipboard.SetContent(dataPackage);
+
+                PasswordButtonTextBlock.Visibility = Visibility.Collapsed;
+                PasswordCopiedFontIcon.Visibility = Visibility.Visible;
+                await Task.Delay(1000);
+                PasswordButtonTextBlock.Visibility = Visibility.Visible;
+                PasswordCopiedFontIcon.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        [GeneratedRegex("^https?:\\/\\/", RegexOptions.IgnoreCase, "zh-CN")]
+        private static partial Regex _urlSchemeRegex();
+
+        /// <summary>
+        /// 点击跳转网址
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void OnClickWebsite(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string url = MainViewModel.Instance.SelectedPassword.Website.Trim();
+                if (!string.IsNullOrWhiteSpace(url))
+                {
+                    if (!_urlSchemeRegex().IsMatch(url))
+                    {
+                        url = "https://" + url;
+                    }
+
+                    if (Uri.TryCreate(url, UriKind.Absolute, out Uri createdUrl))
+                    {
+                        if (createdUrl.Scheme == Uri.UriSchemeHttp || createdUrl.Scheme == Uri.UriSchemeHttps)
+                        {
+                            await Windows.System.Launcher.LaunchUriAsync(createdUrl);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+
     }
 }
--- ManyPasswords/Views/DetailsPage.xaml.cs
+++ ManyPasswords/Views/DetailsPage.xaml.cs
@@ -160,52 +160,6 @@ namespace Honeypot
         }
 
         /// <summary>
-        /// 点击收藏
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void OnClickFavorite(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                ViewModel.AddFavorite(ViewModel.CurrentPassword, true);
-            }
-            catch { }
-        }
-
-        /// <summary>
-        /// 点击取消收藏
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void OnClickDelFavorite(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                ViewModel.RemoveFavorite(ViewModel.CurrentPassword, true);
-            }
-            catch { }
-        }
-
-        /// <summary>
-        /// 删除
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void OnClickDelete(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                FrameworkElement element = sender as FrameworkElement;
-                if (element != null)
-                {
-                    FlyoutBase.ShowAttachedFlyout(element);
-                }
-            }
-            catch { }
-        }
-
-        /// <summary>
         /// 确认删除
         /// </summary>
         /// <param name="sender"></param>
@@ -296,52 +250,6 @@ namespace Honeypot
             catch { }
         }
 
-
-        // 收藏按钮缩放动画
-        private Compositor _compositor = Window.Current.Compositor;
-        private SpringVector3NaturalMotionAnimation _springAnimation;
-
-        private void CreateOrUpdateSpringAnimation(float finalValue)
-        {
-            try
-            {
-                if (_springAnimation == null)
-                {
-                    _springAnimation = _compositor.CreateSpringVector3Animation();
-                    _springAnimation.Target = "Scale";
-                    _springAnimation.DampingRatio = 0.5f;
-                }
-
-                _springAnimation.FinalValue = new Vector3(finalValue);
-            }
-            catch { }
-        }
-
-        private void SpringAnimationPointerEntered(object sender, PointerRoutedEventArgs e)
-        {
-            try
-            {
-                // Scale up to 1.3
-                CreateOrUpdateSpringAnimation(1.3f);
-
-                (sender as UIElement).StartAnimation(_springAnimation);
-            }
-            catch { }
-        }
-
-        private void SpringAnimationPointerExited(object sender, PointerRoutedEventArgs e)
-        {
-            try
-            {
-                // Scale back down to 1.0
-                CreateOrUpdateSpringAnimation(1.0f);
-
-                (sender as UIElement).StartAnimation(_springAnimation);
-            }
-            catch { }
-        }
-
-
         // 鼠标移入，显示密码
         private void Grid_PointerEntered(object sender, PointerRoutedEventArgs e)
         {
@@ -368,24 +276,6 @@ namespace Honeypot
             try
             {
                 bHidePassword = false;
-            }
-            catch { }
-        }
-
-        private void GoingToEnterStoryboard_Completed(object sender, object e)
-        {
-            try
-            {
-                GoingToLeaveStoryboard.Begin();
-            }
-            catch { }
-        }
-
-        private void CantgoEnterStoryboard_Completed(object sender, object e)
-        {
-            try
-            {
-                CantgoLeaveStoryboard.Begin();
             }
             catch { }
         }
