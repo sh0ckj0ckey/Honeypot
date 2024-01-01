9c506149bf09ecaadcdd710f5c0aa3b24bb9d022
姚一鸣 <yaoyiming123@live.com>
2024-01-01 13:12:49 +0800

    m

--- Honeypot/ViewModels/MainViewModel.cs
+++ Honeypot/ViewModels/MainViewModel.cs
@@ -98,5 +98,73 @@ namespace Honeypot.ViewModels
                 Debug.WriteLine(ex.Message);
             }
         }
+
+        // 解锁应用程序
+        public async void UnlockApp()
+        {
+            try
+            {
+                if (AppSettings.EnableLock == false)
+                {
+                    Unlock();
+                    return;
+                }
+
+                switch (await Windows.Security.Credentials.UI.UserConsentVerifier.RequestVerificationAsync("验证您的身份"))
+                {
+                    case Windows.Security.Credentials.UI.UserConsentVerificationResult.Verified:
+                        Unlock();
+                        break;
+                    case Windows.Security.Credentials.UI.UserConsentVerificationResult.DeviceNotPresent:
+                    case Windows.Security.Credentials.UI.UserConsentVerificationResult.NotConfiguredForUser:
+                    case Windows.Security.Credentials.UI.UserConsentVerificationResult.DisabledByPolicy:
+                        await new Windows.UI.Popups.MessageDialog("当前识别设备未配置或被系统策略禁用，请尝试使用密码解锁").ShowAsync();
+                        break;
+                    case Windows.Security.Credentials.UI.UserConsentVerificationResult.DeviceBusy:
+                        await new Windows.UI.Popups.MessageDialog("当前识别设备不可用，请尝试使用密码解锁").ShowAsync();
+                        break;
+                    case Windows.Security.Credentials.UI.UserConsentVerificationResult.RetriesExhausted:
+                        await new Windows.UI.Popups.MessageDialog("验证失败，请尝试使用密码解锁").ShowAsync();
+                        break;
+                    case Windows.Security.Credentials.UI.UserConsentVerificationResult.Canceled:
+                        break;
+                    default:
+                        break;
+                }
+            }
+            catch { }
+        }
+
+        // 开关WindowsHello
+        public async void SetWindowsHelloEnable(bool on)
+        {
+            try
+            {
+                if (AppSettings.EnableLock != on)
+                {
+                    switch (await Windows.Security.Credentials.UI.UserConsentVerifier.RequestVerificationAsync("验证您的身份"))
+                    {
+                        case Windows.Security.Credentials.UI.UserConsentVerificationResult.Verified:
+                            AppSettings.EnableLock = on;
+                            break;
+                        case Windows.Security.Credentials.UI.UserConsentVerificationResult.DeviceNotPresent:
+                        case Windows.Security.Credentials.UI.UserConsentVerificationResult.NotConfiguredForUser:
+                        case Windows.Security.Credentials.UI.UserConsentVerificationResult.DisabledByPolicy:
+                            await new Windows.UI.Popups.MessageDialog("当前识别设备未配置或被系统策略禁用").ShowAsync();
+                            break;
+                        case Windows.Security.Credentials.UI.UserConsentVerificationResult.DeviceBusy:
+                            await new Windows.UI.Popups.MessageDialog("当前识别设备不可用").ShowAsync();
+                            break;
+                        case Windows.Security.Credentials.UI.UserConsentVerificationResult.RetriesExhausted:
+                            await new Windows.UI.Popups.MessageDialog("验证失败").ShowAsync();
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+            catch { }
+        }
+
     }
 }
