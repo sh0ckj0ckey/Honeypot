967aba94b17ea004908f37de3625876cacbd213f
sh0ckj0ckey <yaoyiming123@live.com>
2026-07-02 11:50:07 +0800

    Clean up code to better support null safety and Native AOT

--- Honeypot/App.xaml.cs
+++ Honeypot/App.xaml.cs
@@ -1,8 +1,10 @@
-﻿// Copyright (c) Microsoft Corporation and Contributors.
-// Licensed under the MIT License.
-
-using Microsoft.UI.Xaml;
-using WinUIEx;
+﻿using Microsoft.UI.Xaml;
+using Microsoft.Windows.AppLifecycle;
+using Microsoft.Windows.AppNotifications;
+using Microsoft.Windows.AppNotifications.Builder;
+using Windows.ApplicationModel.Activation;
+using Windows.Win32;
+using Windows.Win32.Foundation;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -16,7 +18,11 @@ namespace Honeypot
     {
         private readonly Microsoft.UI.Dispatching.DispatcherQueue _dispatcherQueue;
 
-        public static WindowEx MainWindow { get; } = new MainWindow();
+        private static string? _launchArguments = null;
+
+        internal static MainWindow? MainWindow { get; private set; } = null;
+
+        internal static Helpers.SettingsService Settings { get; } = new Helpers.SettingsService();
 
         /// <summary>
         /// Initializes the singleton application object.  This is the first line of authored code
@@ -24,14 +30,23 @@ namespace Honeypot
         /// </summary>
         public App()
         {
-            // Microsoft.Windows.Globalization.ApplicationLanguages.PrimaryLanguageOverride = "zh-CN";
-            // Microsoft.Windows.Globalization.ApplicationLanguages.PrimaryLanguageOverride = "en-US";
+            _dispatcherQueue = Microsoft.UI.Dispatching.DispatcherQueue.GetForCurrentThread();
 
             this.InitializeComponent();
 
-            _dispatcherQueue = Microsoft.UI.Dispatching.DispatcherQueue.GetForCurrentThread();
+            UnhandledException += (s, e) =>
+            {
+                e.Handled = true;
 
-            UnhandledException += (s, e) => { e.Handled = true; };
+                var notification = new AppNotificationBuilder()
+                .AddText("An exception was thrown.")
+                .AddText($"Type: {e.Exception.GetType()}")
+                .AddText($"Message: {e.Message}\r\n" +
+                         $"HResult: {e.Exception.HResult}")
+                .BuildNotification();
+
+                AppNotificationManager.Default.Show(notification);
+            };
         }
 
         /// <summary>
@@ -40,15 +55,27 @@ namespace Honeypot
         /// <param name="args">Details about the launch request and process.</param>
         protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
         {
+            var activationArgs = AppInstance.GetCurrent().GetActivatedEventArgs();
+            if (activationArgs is not null && activationArgs.Kind == ExtendedActivationKind.Launch && activationArgs.Data is ILaunchActivatedEventArgs launchArgs)
+            {
+                _launchArguments = launchArgs.Arguments?.Trim();
+            }
+
+            MainWindow ??= new MainWindow();
             MainWindow.Activate();
         }
 
-        public void ShowMainWindow()
+        internal void HandleRedirectedActivation(AppActivationArguments args)
         {
             _dispatcherQueue.TryEnqueue(() =>
             {
-                MainWindow.Restore();
-                MainWindow.BringToFront();
+                MainWindow ??= new MainWindow();
+
+                HWND hWnd = new(WinRT.Interop.WindowNative.GetWindowHandle(MainWindow));
+                PInvoke.ShowWindow(hWnd, Windows.Win32.UI.WindowsAndMessaging.SHOW_WINDOW_CMD.SW_RESTORE);
+                PInvoke.SetForegroundWindow(hWnd);
+
+                MainWindow.Activate();
             });
         }
     }
--- Honeypot/Program.cs
+++ Honeypot/Program.cs
@@ -2,54 +2,54 @@
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.UI.Dispatching;
+using Microsoft.UI.Xaml;
 using Microsoft.Windows.AppLifecycle;
 
-namespace Honeypot
+namespace Honeypot;
+
+public static class Program
 {
-    public static class Program
+    private static App? _app;
+
+    [STAThread]
+    public static void Main(string[] _)
     {
-        private static App _app;
+        WinRT.ComWrappersSupport.InitializeComWrappers();
 
-        [STAThread]
-        public static void Main(string[] args)
+        if (DecideRedirectionAsync().Result == true)
         {
-            WinRT.ComWrappersSupport.InitializeComWrappers();
-
-            var isRedirect = DecideRedirection().GetAwaiter().GetResult();
-
-            if (!isRedirect)
-            {
-                Microsoft.UI.Xaml.Application.Start((p) =>
-                {
-                    var dispatcherQueue = DispatcherQueue.GetForCurrentThread();
-                    var context = new DispatcherQueueSynchronizationContext(dispatcherQueue);
-                    SynchronizationContext.SetSynchronizationContext(context);
-                    _app = new App();
-                });
-            }
+            return;
         }
 
-        private static async Task<bool> DecideRedirection()
+        Application.Start((p) =>
         {
-            AppInstance keyInstance = AppInstance.FindOrRegisterForKey("HONEYPOT-C04ACD32-2B0A-46F0-BCE5-C17601EF5579");
-            AppActivationArguments args = AppInstance.GetCurrent().GetActivatedEventArgs();
+            var context = new DispatcherQueueSynchronizationContext(DispatcherQueue.GetForCurrentThread());
+            SynchronizationContext.SetSynchronizationContext(context);
+            _app = new App();
+        });
+    }
 
-            bool isRedirect = false;
-            if (keyInstance.IsCurrent)
-            {
-                keyInstance.Activated += OnActivated;
-            }
-            else
-            {
-                isRedirect = true;
-                await keyInstance.RedirectActivationToAsync(args);
-            }
-            return isRedirect;
+    private static async Task<bool> DecideRedirectionAsync()
+    {
+        bool isRedirect = false;
+        AppActivationArguments args = AppInstance.GetCurrent().GetActivatedEventArgs();
+        AppInstance keyInstance = AppInstance.FindOrRegisterForKey("NoMewing.Honeypot");
+
+        if (keyInstance.IsCurrent)
+        {
+            keyInstance.Activated += Program_Activated;
+        }
+        else
+        {
+            isRedirect = true;
+            await keyInstance.RedirectActivationToAsync(args);
         }
 
-        private static void OnActivated(object sender, AppActivationArguments args)
-        {
-            _app?.ShowMainWindow();
-        }
+        return isRedirect;
+    }
+
+    private static void Program_Activated(object? sender, AppActivationArguments args)
+    {
+        _app?.HandleRedirectedActivation(args);
     }
 }
